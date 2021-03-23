19438d31ccf480d0f9171a10f1827f4e7f4e4b1b
Erick Rincones <erick.rincones@ciens.ucv.ve>
2021-03-22 20:37:25 -0400

    Update packages
    Use extended color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require(`tailwindcss/colors`);
+
 module.exports = {
   purge: {
     mode: `all`,
@@ -8,6 +10,7 @@ module.exports = {
   },
   darkMode: `class`,
   theme: {
+    colors,
     extend: {}
   },
   variants: {},
